cefd9791c273fe7ccef68699fbd568e9a0465315
yim0327 <yim0327@naver.com>
2025-10-27 21:09:58 +0900

    feat(GameManager): 콘솔I/O 및 서비스 로직을 활용하여 컨트롤러 구현
    - depth를 줄이기 위해 메서드를 쪼갬
    - start() : 레이싱게임을 위한 차량 준비
    - playGame() : 입력받은 횟수 만큼 라운드 진행
    - playTurn() : 각 라운드 별 자동차 이동 수행

--- src/main/java/racingcar/controller/GameManager.java
+++ src/main/java/racingcar/controller/GameManager.java
@@ -1,5 +1,7 @@
 package racingcar.controller;
 
+import racingcar.domain.RacingCar;
+import racingcar.domain.RegisteredCarList;
 import racingcar.service.RacingGame;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -11,12 +13,30 @@ public class GameManager {
 
     public GameManager(InputView inputView, OutputView outputView, RacingGame racingGame) {
         this.inputView = inputView;
-        this.racingGame = racingGame;
         this.outputView = outputView;
+        this.racingGame = racingGame;
     }
 
     public void start() {
-        racingGame.preparateVehicle(inputView.inputCarNames());
+        RegisteredCarList carList =  racingGame.preparateVehicle(inputView.inputCarNames());
+        playGame(inputView.inputTryCount(), carList);
+    }
+
+    private void playGame(int tryCount, RegisteredCarList carList) {
+        outputView.printExecutionResults();
+        for (int i = 0; i < tryCount; i++) {
+            playTurn(carList);
+            System.out.println();
+        }
+
+        outputView.printWinners(racingGame.judge(carList));
+    }
+
+    private void playTurn(RegisteredCarList carList) {
+        for (RacingCar car : carList.getCarList()) {
+            racingGame.playRound(car);
+            outputView.printGameRecord(car);
+        }
     }
 
 }
