c25ba6f3145fac83607e591a33ca440b7259fffd
yim0327 <yim0327@naver.com>
2025-10-27 21:42:20 +0900

    feat(CarRegistration): 자동차 이름 중복 예외 처리 추가
    - HashSet을 활용하여 차량 이름 중복 예외처리 추가

--- src/main/java/racingcar/domain/CarRegistration.java
+++ src/main/java/racingcar/domain/CarRegistration.java
@@ -1,6 +1,8 @@
 package racingcar.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarRegistration {
     private final RegisteredCarList registeredCarList;
@@ -10,10 +12,23 @@ public class CarRegistration {
     }
 
     public RegisteredCarList registrate(List<String> CarNames) {
+        validateDuplicate(CarNames);
+
         for (String carName : CarNames) {
             registeredCarList.add(RacingCar.from(carName));
         }
 
         return registeredCarList;
     }
+
+    private void validateDuplicate(List<String> CarNames) {
+        Set<String> uniqueNames = new HashSet<>();
+
+        for (String name : CarNames) {
+            if (!uniqueNames.add(name.trim())) {
+                throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+            }
+        }
+    }
+
 }
