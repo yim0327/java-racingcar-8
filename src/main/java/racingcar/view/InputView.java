7a974cfa40a083501fcea849cd4b05c02c9ff759
yim0327 <yim0327@naver.com>
2025-10-27 21:21:24 +0900

    feat(InputView): 입력값 검증 로직 추가
    - 자동차 이름
      - validateNonEmptyList() : 적어도 하나의 자동차를 입력
    
    - 시도 횟수
      - validateTryCount() : 시도 횟수는 최소 1 이상
      - validateNotBlank() : 입력값은 비어 있거나 공백 불가
      - validateIsNumeric() : 오직 숫자만 입력 가능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,15 +10,48 @@ public class InputView {
 
     public List<String> inputCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Arrays.stream(Console.readLine().split(","))
+        List<String> carNames = Arrays.stream(Console.readLine().split(","))
                 .map(String::trim)
+                .filter(s -> !s.isEmpty())
                 .collect(Collectors.toList());
+
+        validateNonEmptyList(carNames);
+        return carNames;
     }
 
     public int inputTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String tryCount = Console.readLine();
+        validateTryCount(tryCount);
         return Integer.parseInt(tryCount);
+    }
+
+    private void validateNonEmptyList(List<String> input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("적어도 하나의 자동차를 입력해야 합니다.");
+        }
+    }
+
+    private void validateTryCount(String input) {
+        validateNotBlank(input);
+        validateIsNumeric(input);
+
+        int tryCount = Integer.parseInt(input);
+        if (tryCount <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 최소 1 이상이어야 합니다.");
+        }
+    }
+
+    private void validateNotBlank(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력값은 비어 있거나 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateIsNumeric(String input) {
+        if (!input.matches("[0-9]+")) {
+            throw new IllegalArgumentException("오직 숫자만 입력 가능합니다");
+        }
     }
 
 }
